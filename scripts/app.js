145dee4b9c4be48fdd65138bb30fdabd98f1802e
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-23 14:48:10 +0300

    Able to get a response from jQuery

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,17 @@
 const title = document.getElementById('title');
 const date = document.getElementById('date');
 const article = document.getElementById('news-article');
+const articlesLink = 'http://localhost/news-web-bootstrap/api/getArticles.php'
 
-const $ = require( "jquery" );
+
+// const $ = require( "jquery" );
+// getJSON returns an array of objects
+$.getJSON(articlesLink, (e)=>{
+    // console.log(e);
+
+    res = e;
+    console.log(res[0].id)
+})
 
 
 
